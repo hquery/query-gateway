ec0ba4e49b59f5c0ef927ab7f97616f4ff3697de
Andy Gregorowicz <andy@mitre.org>
2011-06-01 15:18:50 -0400

    Generated Rails 3.1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QueryGateway::Application.config.secret_token = 'ce9e8c0d08a672efb7e580ae5ed1430582d8129b1802ca3b6f3a7d2b76b71a6215684401f2e925974bdc32a92374c3f0700ff5757c7e2ccfdbb70b28ab0f5d81'
