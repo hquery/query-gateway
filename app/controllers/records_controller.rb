ce33f07342554bb380c0232586c2d28a508fa43e
Glen McCallum <glen.mccallum@nextinformatics.com>
2012-05-31 13:29:00 -0700

    Added support for importing a Scoop basic XML document

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -3,12 +3,23 @@ class RecordsController < ApplicationController
     xml_file = params[:content].read
     doc = Nokogiri::XML(xml_file)
     doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
-    pi = HealthDataStandards::Import::C32::PatientImporter.instance
-    patient = pi.parse_c32(doc)
 
-    patient.save!
+    if params[:content_type]== 'c32'
+	    pi = HealthDataStandards::Import::C32::PatientImporter.instance
+			patient = pi.parse_c32(doc)
+			patient.save!
+			render :text => 'Patient imported', :status => 201
 
-    render :text => 'Patient imported', :status => 201
+    elsif params[:content_type] == 'scoop_basic'
+    	pi = HealthDataStandards::Import::ScoopBasic::PatientImporter.instance
+    	patient = pi.parse_scoop_basic(doc)
+    	patient.save!
+    	render :text => 'Patient imported', :status => 201
+    
+    else
+    	render :text => 'Patient not imported', :status => 422
+    end
+    
   end
 
 end
--- test/functional/records_controller_test.rb
+++ test/functional/records_controller_test.rb
@@ -6,13 +6,22 @@ class RecordsControllerTest < ActionController::TestCase
     dump_database
   end
   
-  test "should POST a C32 to create" do
+   test "should POST a C32 to create" do
     c32 = Rack::Test::UploadedFile.new(File.join(Rails.root, 'test', 'fixtures', 'TobaccoUser0028.xml'), 'application/xml')
-    post :create, {:content => c32}
+    post :create, {:content => c32, :content_type => 'c32'}
     assert_response 201
     r = Mongoid.master['records'].find_one({:first => 'Unit', :last => 'Test'})
     assert r
     assert_equal -773020800, r['birthdate']
+  end
+
+   test "should POST a Scoop Basic XML to create" do
+    c32 = Rack::Test::UploadedFile.new(File.join(Rails.root, 'test', 'fixtures', 'Sample-SCOOP-IT0-Document.xml'), 'application/xml')
+    post :create, {:content => c32, :content_type => 'scoop_basic'}
+    assert_response 201
+    r = Mongoid.master['records'].find_one({:first => 'Henry', :last => 'Levin'})
+    assert r
+    assert_equal -1176163200, r['birthdate']
   end
 
 end
