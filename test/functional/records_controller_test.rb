d8e0d55949454319a2d069199363c4937cce1d02
Raymond Rusk <rusk.raymond@gmail.com>
2013-04-10 13:55:32 -0700

    Reenabled the c32 tests

--- test/functional/records_controller_test.rb
+++ test/functional/records_controller_test.rb
@@ -6,19 +6,18 @@ class RecordsControllerTest < ActionController::TestCase
     dump_database
   end
   
-   #test "should POST a C32 to create" do
-   # c32 = Rack::Test::UploadedFile.new(File.join(Rails.root, 'test', 'fixtures', 'TobaccoUser0028.xml'), 'application/xml')
-   # post :create, {:content => c32}
-   # assert_response 201
-   # r = Mongoid.master['records'].find_one({:first => 'Unit', :last => 'Test'})
-   
-   # assert r
-   # assert_equal -773020800, r['birthdate']
-  #end
-
-   test "should POST a E2E to create" do
-    c32 = Rack::Test::UploadedFile.new(File.join(Rails.root, 'test', 'fixtures', 'JOHN_CLEESE_1_25091940.xml'), 'application/xml')
+  test "should POST a C32 to create" do
+    c32 = Rack::Test::UploadedFile.new(File.join(Rails.root, 'test', 'fixtures', 'TobaccoUser0028.xml'), 'application/xml')
     post :create, {:content => c32}
+    assert_response 201
+    r = Mongoid.master['records'].find_one({:first => 'Unit', :last => 'Test'})
+    assert r
+    assert_equal -773020800, r['birthdate']
+  end
+
+  test "should POST a E2E to create" do
+    e2e = Rack::Test::UploadedFile.new(File.join(Rails.root, 'test', 'fixtures', 'JOHN_CLEESE_1_25091940.xml'), 'application/xml')
+    post :create, {:content => e2e}
     assert_response 201
     r = Mongoid.master['records'].find_one({:first => 'JOHN', :last => 'CLEESE'})
     assert r
