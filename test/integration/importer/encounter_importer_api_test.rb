95a5e8eae95a5f58d0f0b76b7bfab36e61bf2d57
Raymond Rusk <rusk.raymond@gmail.com>
2014-05-29 12:28:11 -0700

    Example of retrieving encounter time

--- test/integration/importer/encounter_importer_api_test.rb
+++ test/integration/importer/encounter_importer_api_test.rb
@@ -25,10 +25,13 @@ class E2EEncounterImporterApiTest < E2EImporterApiTest
     assert_equal 'doctor', @context.eval('e2e_patient.encounters()[0].performer()["json"]["given_name"]')
     assert_equal 'oscardoc', @context.eval('e2e_patient.encounters()[0].performer()["json"]["family_name"]')
     assert_equal '999998', @context.eval('e2e_patient.encounters()[0].performer()["json"]["npi"]')
+    #TODO resolve why time has been resolved to midnight plus 7 hours (seems to be UTC midnight rather than time actually specified)
+    assert_equal Time.gm(2013,9,26,7,0,0).to_i, @context.eval('e2e_patient.encounters()[0].performer()["json"]["start"]')
     assert_nil @context.eval('e2e_patient.encounters()[0].facility().name()')
     assert @context.eval('e2e_patient.encounters()[0].reasonForVisit().includesCodeFrom({"ObservationType-CA-Pending": ["REASON"]})')
     assert_nil @context.eval('e2e_patient.encounters()[0].admitType()')
     assert_equal 2013, @context.eval('e2e_patient.encounters()[0].date().getUTCFullYear()')
+    # Note month count is 0 through 11 so 8 is actually September.
     assert_equal 8, @context.eval('e2e_patient.encounters()[0].date().getUTCMonth()')
     assert_equal 26, @context.eval('e2e_patient.encounters()[0].date().getUTCDate()')
   end
