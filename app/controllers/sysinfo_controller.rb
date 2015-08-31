f6be30e096640dc5df19c121a9f029f269b96461
Raymond Rusk <rusk.raymond@gmail.com>
2015-08-31 16:13:17 -0700

    Specify relative path to Nagios plugins

--- app/controllers/sysinfo_controller.rb
+++ app/controllers/sysinfo_controller.rb
@@ -1,24 +1,31 @@
 class SysinfoController < ApplicationController
+
+  if defined?(Rails.root)
+    NAGIOS_PLUGINS = "#{Rails.root}/nagios-plugins"
+  else
+    NAGIOS_PLUGINS = File.dirname(__FILE__)+'/../../nagios-plugins'
+  end
+
   def currentload
-    render :text => `nagios-plugins/check_load --warning='5.0,4.0,3.0' --critical='10.0,6.0,4.0'`, :status =>201
+    render :text => `#{NAGIOS_PLUGINS}/check_load --warning='5.0,4.0,3.0' --critical='10.0,6.0,4.0'`, :status =>201
   end
 
   def currentusers
-    render :text => `nagios-plugins/check_users -w '5' -c '10'`, :status =>201
+    render :text => `#{NAGIOS_PLUGINS}/check_users -w '5' -c '10'`, :status =>201
   end
 
   def diskspace
-    render :text => `nagios-plugins/check_disk -w '20%' -c '10%' -e`, :status =>201
+    render :text => `#{NAGIOS_PLUGINS}/check_disk -w '20%' -c '10%' -e`, :status =>201
   end
 
   def mongo
   end
 
   def totalprocesses
-    render :text => `nagios-plugins/check_procs -w '250' -c '400'`, :status =>201
+    render :text => `#{NAGIOS_PLUGINS}/check_procs -w '250' -c '400'`, :status =>201
   end
 
   def swap
-    render :text => `nagios-plugins/check_swap -w '95%' -c '90%'`, :status =>201
+    render :text => `#{NAGIOS_PLUGINS}/check_swap -w '95%' -c '90%'`, :status =>201
   end
 end
