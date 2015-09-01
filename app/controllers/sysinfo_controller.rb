3b548925c0a68a9d648cd21d52eddc034c52729b
Raymond Rusk <rusk.raymond@gmail.com>
2015-09-01 14:23:09 -0700

    Capture exit status of Nagios check

--- app/controllers/sysinfo_controller.rb
+++ app/controllers/sysinfo_controller.rb
@@ -7,25 +7,35 @@ class SysinfoController < ApplicationController
   end
 
   def currentload
-    render :text => `#{NAGIOS_PLUGINS}/check_load --warning='5.0,4.0,3.0' --critical='10.0,6.0,4.0'`, :status =>201
+    textstr = `#{NAGIOS_PLUGINS}/check_load --warning='5.0,4.0,3.0' --critical='10.0,6.0,4.0'`
+    textstr += 'Status Code: ' + $?.exitstatus.to_s + "\n"
+    render :text => textstr, :status =>201
   end
 
   def currentusers
-    render :text => `#{NAGIOS_PLUGINS}/check_users -w '5' -c '10'`, :status =>201
+    textstr = `#{NAGIOS_PLUGINS}/check_users -w '5' -c '10'`
+    textstr += 'Status Code: ' + $?.exitstatus.to_s + "\n"
+    render :text => textstr, :status =>201
   end
 
   def diskspace
-    render :text => `#{NAGIOS_PLUGINS}/check_disk -w '20%' -c '10%' -e`, :status =>201
+    textstr = `#{NAGIOS_PLUGINS}/check_disk -w '20%' -c '10%' -e`
+    textstr += 'Status Code: ' + $?.exitstatus.to_s + "\n"
+    render :text => textstr, :status =>201
   end
 
   def mongo
   end
 
   def totalprocesses
-    render :text => `#{NAGIOS_PLUGINS}/check_procs -w '250' -c '400'`, :status =>201
+    textstr = `#{NAGIOS_PLUGINS}/check_procs -w '250' -c '400'`
+    textstr += 'Status Code: ' + $?.exitstatus.to_s + "\n"
+    render :text => textstr, :status =>201
   end
 
   def swap
-    render :text => `#{NAGIOS_PLUGINS}/check_swap -w '95%' -c '90%'`, :status =>201
+    textstr = `#{NAGIOS_PLUGINS}/check_swap -w '95%' -c '90%'`
+    textstr += 'Status Code: ' + $?.exitstatus.to_s + "\n"
+    render :text => textstr, :status =>201
   end
 end
